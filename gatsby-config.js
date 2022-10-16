e4db4b1644906f6d70b779de728c1fa6861064d4
Val Paliy <valpaliy@gmail.com>
2022-10-16 11:01:40 +0300

    Config modified

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Valticus",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Playground for new features @ valticus.pro",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
