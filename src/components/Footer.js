94304bd05f2745fef9659bdbc0fbd24bb50a169c
Val Paliy <valpaliy@gmail.com>
2022-10-17 13:06:04 +0300

    Logo updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,9 +10,6 @@ import github from "../img/github-icon.svg";
 const Footer = () => {
   return (
     <footer className="footer has-background-black has-text-white-ter">
-      <div className="content has-text-centered">
-        <img src={logo} alt="Kaldi" style={{ width: "14em", height: "10em" }} />
-      </div>
       <div className="content has-text-centered has-background-black has-text-white-ter">
         <div className="container has-background-black has-text-white-ter">
           <div style={{ maxWidth: "100vw" }} className="columns">
